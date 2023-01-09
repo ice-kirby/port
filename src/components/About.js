072f1dfc67bf6a083602330f341c42055cad9046
naeun <gongwillson3@gmail.com>
2023-01-09 14:05:13 +0900

    real final port

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../style/About.scss";
+import { AiFillCaretRight } from "react-icons/ai";
 
 const About = () => {
   return (
@@ -28,16 +29,33 @@ const About = () => {
           2017.08 ~ 2019.07
           <br />
           <span className="Aimage">이미지프레임</span> 근무
+          <p>블랑시아팀 사원</p>
+          <p className="Aword-p">
+            <AiFillCaretRight />
+            작가 컨택, 작품 관리, 이벤트 기획, 사이트 & SNS 관리
+          </p>
         </div>
         <div className="Aword ArightTwo">
           2020.07 ~ 2022.05
           <br />
           <span className="AJoara">조아라</span> 근무
+          <p>퍼블리싱팀 사원</p>
+          <p className="Aword-p">
+            <AiFillCaretRight />
+            EPUB 파일 제작, 연재 작품 유통, 공모전 작품 검토{" "}
+          </p>
         </div>
         <div className="Aword ArightThree">
           2022.10 ~ ing
           <br />
-          <span className="Asesac">청년취업사관학교 SeSAC </span>시작
+          <span className="Asesac">청년취업사관학교 SeSAC </span>
+          <p>기업연계형 프론트엔드 실무 프로젝트 과정 수강</p>
+          <p className="Aword-p">
+            <AiFillCaretRight />
+            HTML, CSS, JS, TS, React 등 프론트엔드 개발 기술 학습 <br />
+            <AiFillCaretRight />
+            (주)모닛 홈페이지 리뉴얼 팀 프로젝트 개발 참여
+          </p>
         </div>
       </div>
     </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -17,18 +17,26 @@ const Project = () => {
           </div>
         </Link>
         <div className="ProWord">
+          <p>&lt; 오늘 뭐 입어? &gt;</p>
+          <br />
           <p>
-            오늘 입고 나갈 옷과 모자를 정해보자. 옷을 확정하면 현재 기온과
-            적절히 맞는 차림인지 체크해주고 기온과 맞는 옷을 추천해준다.
+            - 프로젝트 소개 : 오늘 입고 나갈 옷과 모자를 정할 수 있다. 옷을
+            확정하면 현재 기온과 적절히 맞는 차림인지 체크하고, 기온에 맞는 옷을
+            추천해준다.
           </p>
           <br />
+          <span>- 활용 기술 : </span>HTML, CSS, JAVASCRIPT
           <br />
-          <span>사용기술 : </span>HTML, CSS, JAVASCRIPT
           <br />
-          <br />
-          <span>개발사항 </span>: 날씨 API를 사용해 현재 기온을 불러왔다. 고른
-          옷의 점수를 계산하여 로컬스토리지에 저장 후 날씨 점수를 산정해 둘을
-          비교하게 만들었다.
+          <div className="ProWord-box">
+            <span>- 개발 주요 사항 </span>
+            <div>
+              <p>▷ 날씨 API를 사용해 현재 기온을 불러와 표시</p>
+              <p>▷ 고른 옷의 점수를 계산해 로컬스토리지에 저장</p>
+              <p>▷ 옷의 점수와 현재 기온 점수를 비교해 결과 산출</p>
+              <p>▷ CDN으로 alert창 스타일 적용</p>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -47,17 +55,29 @@ const Project = () => {
           </figure>
         </div>
         <div className="ProWord">
+          <p>&lt; Todo-list &gt;</p>
+          <br />
           <p>
-            아이스크림 트럭 컨셉으로 제작한 Todo-app. 민트초코는 현재의 할 일,
-            블루레몬에이드는 완료된 칸이다.
+            - 프로젝트 소개 : 아이스크림 트럭 컨셉으로 제작한 Todo-app.
+            민트초코는 현재의 할 일, 블루레몬에이드는 완료된 칸이다.
           </p>
           <br />
+          <span>- 활용 기술 : </span>HTML, CSS, JAVASCRIPT, REACT
           <br />
-          <span>사용기술 : </span>HTML, CSS, JAVASCRIPT, REACT
           <br />
-          <br />
-          <span>개발사항 : </span>map을 이용해 할 일 목록의 완료 처리와 삭제
-          버튼을 작동시켰다. 완료 목록의 filter로 목록을 월별로 확인할 수 있다.
+          <div className="ProWord-box">
+            <span>- 개발 주요 사항 </span>
+            <div>
+              <p>▷ 삭제 버튼을 누르면 map함수를 이용해 목록에서 삭제</p>
+              <p>
+                ▷ 체크박스를 누르면 map함수를 이용해 완료 목록으로 해당 항목
+                이동
+              </p>
+              <p>
+                ▷ 완료 목록 칸의 filter를 통해 완료 항목을 월별로 확인 가능{" "}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -74,18 +94,25 @@ const Project = () => {
           </figure>
         </div>
         <div className="ProWord">
+          <p>&lt; MY diary &gt;</p>
+          <br />
           <p>
-            "캐릭캐릭체인지" 컨셉으로 제작한 일기장. 저장한 일기는 왼쪽 버튼의
-            일기 목록에서 확인 가능하다.
+            - 프로젝트 소개 : "캐릭캐릭체인지" 컨셉으로 제작한 일기장. 저장한
+            일기는 왼쪽 버튼의 일기 목록에서 확인 가능하다.
           </p>
           <br />
+          <span>- 활용 기술 : </span>HTML, SCSS, JAVASCRIPT, REACT
           <br />
-          <span>사용기술 : </span>HTML, SCSS, JAVASCRIPT, REACT
           <br />
-          <br />
-          <span>개발사항 : </span>router를 이용해 일기 입력 칸과 일기 목록을
-          분리했다. css 애니메이션으로 "캐릭캐릭체인지"의 마스코트들이
-          움직이도록 만들었다.
+          <div className="ProWord-box">
+            <span>- 개발 주요 사항 </span>
+            <div>
+              <p>▷ react-router-dom을 이용해 입력 칸과 일기 목록을 분리</p>
+              <p>▷ keyframes으로 마스코트들의 애니메이션 구현</p>
+              <p>▷ @mixin으로 scss 활용</p>
+              <p>▷ useEffect 사용으로 최적화 시도</p>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -99,18 +126,23 @@ const Project = () => {
           </figure>
         </div>
         <div className="ProWord">
+          <p>&lt; Monit-Mecs &gt;</p>
+          <br />
           <p>
-            SeSAC에서 진행 중인 팀 프로젝트. monit 사이트의 전체적인 수정을
-            맡았다.
+            - 프로젝트 소개 : SeSAC에서 진행 중인 팀 프로젝트. monit 사이트의
+            전체적인 수정을 맡았다.
           </p>
           <br />
+          <span>- 활용 기술 : </span>HTML, SCSS, JAVASCRIPT, REACT
           <br />
-          <span>사용기술 : </span>HTML, SCSS, JAVASCRIPT, REACT
           <br />
-          <br />
-          <span>개발사항 : </span>기존 자바스크립트로 짜여진 사이트를 React -
-          router로 개편하고 있다. key-color 수정 및 추가하고, 사이트 내
-          애니메이션을 추가할 예정이다.
+          <div className="ProWord-box">
+            <span>- 개발 주요 사항 </span>
+            <div>
+              <p>▷ 자바스크립트로 구현된 페이지를 React - router로 개편 시도</p>
+              <p>▷ key-color 수정 및 추가와 사이트 내 애니메이션 추가</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
